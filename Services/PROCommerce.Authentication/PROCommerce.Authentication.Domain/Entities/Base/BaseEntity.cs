de08ae580b9b97a822ef9dbbdeff0fda4a60e629
Flávio Eduardo <xflax00@gmail.com>
2024-09-11 23:59:55 -0300

    feat(entities): Criando entidade User e BaseEntity.

---
+++ Services/PROCommerce.Authentication/PROCommerce.Authentication.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿namespace PROCommerce.Authentication.Domain.Entities.Base;
+
+public abstract class BaseEntity
+{
+    private long? _id = null;
+
+    public long? Id 
+    {
+        get
+        {
+            return _id;
+        }
+        set
+        {
+            _id = value;
+        }
+    }
+
+    public DateTime? CreationDate { get; set; }
+
+    public DateTime? UpdateDate { get; set; }
+
+    public DateTime? DeletionDate { get; set; }
+
+    protected void SetIdentity(long id)
+    {
+        _id = id;
+    }
+}
---
+++ Services/PROCommerce.Authentication/PROCommerce.Authentication.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using PROCommerce.Authentication.Domain.Entities.Base;
+
+namespace PROCommerce.Authentication.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string? Name { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Email { get; set; }
+}
