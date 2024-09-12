64d9246a963bceb407c0ac2f8de4b7ce8d7cf03d
Flávio Eduardo <xflax00@gmail.com>
2024-09-12 00:01:13 -0300

    feat(repository): Implementando o o DbContext persistência e obtenção de dados.

---
+++ Services/PROCommerce.Authentication/PROCommerce.Authentication.Infrastructure/Repository/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PROCommerce.Authentication.Domain.Entities;
+
+namespace PROCommerce.Authentication.Infrastructure.Repository;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
