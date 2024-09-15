0e0297bfc71dff8c4c60685fe43241fa49dd8d5b
Flávio Eduardo <xflax00@gmail.com>
2024-09-15 15:54:05 -0300

    feat(structure): Implementando endpoint de login no controller de autenticação.

---
+++ Services/PROCommerce.Authentication/PROCommerce.Authentication.API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PROCommerce.Authentication.API.Models.Auth.Login;
+using PROCommerce.Authentication.Domain.DTOs.Auth;
+using PROCommerce.Authentication.Domain.DTOs.Auth.Response;
+using PROCommerce.Authentication.Domain.Interfaces.Services;
+
+namespace PROCommerce.Authentication.API.Controllers;
+
+[Route("api")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthServices _authServices;
+
+    public AuthController(IAuthServices authServices)
+    {
+        _authServices = authServices;
+    }
+
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public IActionResult Login(LoginModel loginModel)
+    {
+
+        LoginDTO loginDTO = loginModel.Adapt<LoginDTO>();
+
+        LoginResponseDTO loginResponseDTO = _authServices.Login(loginDTO);
+
+        return StatusCode(StatusCodes.Status200OK, loginResponseDTO);
+    }
+}
