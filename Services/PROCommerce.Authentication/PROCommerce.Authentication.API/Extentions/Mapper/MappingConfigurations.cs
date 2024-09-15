876c6ef1ccfa78a382e07449158796a62fd7220f
Flávio Eduardo <xflax00@gmail.com>
2024-09-15 15:49:28 -0300

    feat(mapping): Implemetando mapster para mapeamento de dados.

---
+++ Services/PROCommerce.Authentication/PROCommerce.Authentication.API/Extentions/Mapper/MappingConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+using PROCommerce.Authentication.Domain.DTOs.Auth.Response;
+using PROCommerce.Authentication.Domain.Entities;
+using System.Reflection;
+
+namespace PROCommerce.Authentication.API.Extentions.Mapper;
+
+public static class MappingConfigurations
+{
+    public static void RegisterMaps(this IServiceCollection services)
+    {
+        TypeAdapterConfig<(User, string token), LoginResponseDTO>
+            .NewConfig()
+            .Map(dest => dest.Id, src => src.Item1.Id)
+            .Map(dest => dest.Token, src => src.token);
+
+        TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+    }
+}
