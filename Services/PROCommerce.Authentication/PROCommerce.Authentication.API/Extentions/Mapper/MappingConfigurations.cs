7b3c37a3cdbc4e24457b4a0fb811be6f2ca0def1
Flávio Eduardo <xflax00@gmail.com>
2024-12-12 15:30:12 -0300

    feat(mapping): Mapeando entidades e DTOs.

--- Services/PROCommerce.Authentication/PROCommerce.Authentication.API/Extentions/Mapper/MappingConfigurations.cs
+++ Services/PROCommerce.Authentication/PROCommerce.Authentication.API/Extentions/Mapper/MappingConfigurations.cs
@@ -1,4 +1,5 @@
 ﻿using Mapster;
+using PROCommerce.Authentication.Domain.DTOs.Auth;
 using PROCommerce.Authentication.Domain.DTOs.Auth.Response;
 using PROCommerce.Authentication.Domain.Entities;
 using System.Reflection;
@@ -9,10 +10,20 @@ public static class MappingConfigurations
 {
     public static void RegisterMaps(this IServiceCollection services)
     {
+
+        #region User => LoginResponseDTO
         TypeAdapterConfig<(User, string token), LoginResponseDTO>
             .NewConfig()
             .Map(dest => dest.Id, src => src.Item1.Id)
             .Map(dest => dest.Token, src => src.token);
+        #endregion User => LoginResponseDTO
+
+        #region RegisterDTO, hashPassword => User
+        TypeAdapterConfig<(RegisterDTO registerDTO, string hashPassword), User>
+            .NewConfig()
+            .Map(dest => dest, src => src.registerDTO)
+            .Map(dest => dest.Password, src => src.hashPassword);
+        #endregion RegisterDTO, hashPassword => User
 
         TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
     }
